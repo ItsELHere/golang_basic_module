731e2dad72d844363e507b5c78beca2c55bcc632
Imanuel Antonio Putra <imanuel_ant@Imanuels-MacBook-Pro.local>
2023-08-21 06:52:56 +0700

    Golang basic module

---
+++ Hello.go
@@ -0,0 +1,8 @@
+package golang_basic_module
+
+import "fmt"
+
+func Introduction(name string){
+	fmt.Println("Hello. My name is" + name)
+	fmt.Println("Welcome to my basic golang module")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ItsELHere/golang_basic_module
+
+go 1.21.0
